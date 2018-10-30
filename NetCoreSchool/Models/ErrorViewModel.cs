f58ec3dac5ed829f4d2f8e75940a42464050b6d9
ligc <1491503638@qq.com>
2018-10-30 23:21:54 +0800

    code add

---
+++ NetCoreSchool/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NetCoreSchool.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
